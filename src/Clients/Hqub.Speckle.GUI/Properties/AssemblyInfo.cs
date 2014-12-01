b78711b71d666c0eeae37e26cecb1b469ed53449
Glebov Boris <avatar29A@gmail.com>
2014-12-01 17:54:44 +0500

    Отредактировал св-ва сборки

--- src/Clients/Hqub.Speckle.GUI/Properties/AssemblyInfo.cs
+++ src/Clients/Hqub.Speckle.GUI/Properties/AssemblyInfo.cs
@@ -7,12 +7,14 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hqub.Speckle.GUI")]
-[assembly: AssemblyDescription("")]
+using Hqub.Speckle.GUI;
+
+[assembly: AssemblyTitle(Metadata.Title)]
+[assembly: AssemblyDescription("Speckle analyzing tools")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("H-QUB")]
 [assembly: AssemblyProduct("Hqub.Speckle.GUI")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright h-qub company©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +53,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(Metadata.Version)]
+[assembly: AssemblyFileVersion(Metadata.Version)]
